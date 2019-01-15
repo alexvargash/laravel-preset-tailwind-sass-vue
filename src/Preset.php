4bd854e9b844abde46277f45d04c2d76216a5110
Alex Vargas <avargas@digizent.com>
2019-01-15 11:13:44 -0600

    Updated all the npm dependencies

--- src/Preset.php
+++ src/Preset.php
@@ -32,13 +32,15 @@ class Preset extends LaravelPreset
     {
         return array_merge(
             [
-                'vue-router' => '^3.0.1',
-                'tailwindcss' => '^0.6.6',
-                'laravel-mix' => '^2.1.14',
-                'laravel-mix-purgecss' => '^3.0.0',
+                'vue' => '^2.5.22',
+                'vue-router' => '^3.0.2',
+                'tailwindcss' => '^0.7.3',
+                'laravel-mix' => '^4.0.14',
+                'laravel-mix-purgecss' => '^4.0.0',
                 'laravel-mix-tailwind' => '^0.1.0',
             ],
             Arr::except($packages, [
+                'vue',
                 'jquery',
                 'lodash',
                 'bootstrap',
